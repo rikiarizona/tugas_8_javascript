46b21e22326c53391e1a09778521376ce8861213
rikiarizona <rikiarizona@gmail.com>
2022-08-03 09:36:38 +0700

    TUGAS 8 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,14 @@
+function tokokomputer(){
+    var laptop ={
+        Merk: 'Asus ROG',
+        Ram: '32GB',
+        SSD: '2TB',
+        VGA: 'Nvidia RTX3010ti',
+        Harga: 30000000,
+    }
+    laptop.Prosesor = 'Intel i12'
+    laptop.SSD = '4TB';
+    laptop.Ram = '64GB';
+    console.log(laptop)
+}
+tokokomputer()
